ec947ef49244c72943728024ef2c19c938fd2718
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-01 18:12:42 +0430

    add projects and project urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
+    path('projects/', views.projects, name='projects'),
+    path('project/<str:pk>/', views.project, name='project'),
 
 ]
