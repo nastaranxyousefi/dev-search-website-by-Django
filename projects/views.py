80d4f751108a7aab945ebf1254445d0fd8235846
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-01 18:09:44 +0430

    add project view

--- projects/views.py
+++ projects/views.py
@@ -4,3 +4,5 @@ from django.http import HttpResponse
 def projects(request):
     return HttpResponse('these are our projects')
 
+def project(request, pk):
+    return HttpResponse('single project' +' '+ pk)
