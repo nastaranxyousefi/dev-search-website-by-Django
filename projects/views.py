bf14ee4b893798b4cb237a63d4736606a8d95918
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-01 22:44:37 +0430

    add render for projects and project view

--- projects/views.py
+++ projects/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def projects(request):
-    return HttpResponse('these are our projects')
+    return render(request, 'projects/projects.html')
 
 def project(request, pk):
-    return HttpResponse('single project' +' '+ pk)
+    return render(request, 'projects/single_project.html')
