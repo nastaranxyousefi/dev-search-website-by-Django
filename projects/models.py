5280dc4d82a9da8d4d9171070a78fcc3e00da5de
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-02 17:17:46 +0430

    add Tag model

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     description = models.TextField(null=True, blank=True)
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_code = models.CharField(max_length=2000, null=True, blank=True)
+    tag = models.ManyToManyField('Tag', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
 
@@ -27,3 +28,12 @@ class Review(models.Model):
 
     def __str__(self):
         return self.value
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.name
