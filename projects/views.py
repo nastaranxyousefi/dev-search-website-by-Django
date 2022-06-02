c5e42f359459ed356889f4e7cf850b8cdb48a206
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-02 19:29:17 +0430

    add queries to projects and project views

--- projects/views.py
+++ projects/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
+from .models import Project
 
 def projects(request):
-    return render(request, 'projects/projects.html')
+    all_projects = Project.objects.all()
+    context = {
+        'projects' : all_projects,
+    }
+    return render(request, 'projects/projects.html', context)
 
 def project(request, pk):
+    project_obj = get_object_or_404(Project, pk=pk)
 
     context = {
-
+        'project_obj' : project_obj
     }
     return render(request, 'projects/single_project.html', context)
