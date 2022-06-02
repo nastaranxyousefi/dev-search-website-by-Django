4b871920b7b99d7cdbfe57741889eb7ab4519dd6
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-02 17:10:23 +0430

    add Review model

--- projects/models.py
+++ projects/models.py
@@ -9,3 +9,21 @@ class Project(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
 
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    VOTE_CHOICES = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    #owner=
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=50, choices=VOTE_CHOICES)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
+
+    def __str__(self):
+        return self.value
