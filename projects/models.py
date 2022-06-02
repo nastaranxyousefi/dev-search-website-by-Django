7db19fea64d3b9fe1104bdc66078010d16fd94ba
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-02 17:20:23 +0430

    add changes to project model

--- projects/models.py
+++ projects/models.py
@@ -7,6 +7,8 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_code = models.CharField(max_length=2000, null=True, blank=True)
     tag = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
 
