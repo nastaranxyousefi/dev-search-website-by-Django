ceb4cfded4c547ab78445241784de2a07718635b
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-06-03 13:29:16 +0430

    add url for delete_project view

--- projects/urls.py
+++ projects/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('project/<str:pk>/', views.project, name='project'),
     path('create-project/', views.create_project, name='create_project'),
     path('update-project/<str:pk>/', views.update_project, name='update_project'),
+    path('delete-project/<str:pk>/', views.delete_project, name='delete_project'),
 
 ]
