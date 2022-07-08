d60e8e95167ffd171a6d7242322d942276949395
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-08 12:17:11 +0430

    add username to Profile model

--- users/models.py
+++ users/models.py
@@ -5,6 +5,7 @@ import uuid
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=250, null=True, blank=True)
+    username = models.CharField(max_length=250, null=True, blank=True)
     email = models.EmailField(max_length=500, blank=True, null=True)
     short_intro = models.CharField(max_length=200, blank=True, null=True)
     bio = models.TextField(blank=True, null=True)
