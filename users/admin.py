f1565970ec4da39a538f093ff7e71bbc92c5268e
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-09 16:55:34 +0430

    add Skill model to users app

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Profile
+from .models import Profile, Skill
 
 
 admin.site.register(Profile)
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner',)
--- users/models.py
+++ users/models.py
@@ -19,9 +19,17 @@ class Profile(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
 
-
     def __str__(self):
         return str(self.user.username)
 
+
+class Skill(models.Model):
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, blank=True, null=True)
+    description = models.TextField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
 
 
