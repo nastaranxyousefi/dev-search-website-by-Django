97ad6fb3bc5bb6855610cf18d6887536deb00371
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-09 23:30:18 +0430

    complete user profile page

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import Profile
-
+from projects.models import Project
 
 def profiles(request):
     profiles = Profile.objects.all()
@@ -12,4 +13,14 @@ def profiles(request):
 
 
 def user_profile(request, pk):
-    return render(request, 'users/user-profile.html')
+    profile = get_object_or_404(Profile, pk=pk)
+    top_skills = profile.skill_set.exclude(description__exact="")
+    other_skills = profile.skill_set.filter(description="")
+    projects = Project.objects.filter(owner=profile)
+    context = {
+        'profile' : profile,
+        'top_skills' : top_skills,
+        'other_skills' : other_skills,
+        'projects' : projects,
+    }
+    return render(request, 'users/user-profile.html', context)
