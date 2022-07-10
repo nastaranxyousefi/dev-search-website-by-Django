e20f54636f588a7400e5dc43bf6e64a5f7810879
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-10 17:45:17 +0430

    add signals file to users app

--- users/models.py
+++ users/models.py
@@ -20,7 +20,7 @@ class Profile(models.Model):
     id = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, primary_key=True)
 
     def __str__(self):
-        return str(self.user.username)
+        return str(self.username)
 
 
 class Skill(models.Model):
