23fb4d915d8e77d4e0d43df69d33ff31e76252bf
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-11 18:10:00 +0430

    add url for logout

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.user_profile, name='user-profile'),
     path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
 ]
