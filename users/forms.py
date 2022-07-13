2b4adb73bf002b6520c938fb333e189077b4b0cd
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-14 00:35:49 +0430

    style the registration form

--- users/forms.py
+++ users/forms.py
@@ -8,4 +8,7 @@ class CustomUserCreationForm(UserCreationForm):
         fields = ['first_name', 'last_name', 'email',
                   'username', 'password1', 'password2']
 
-
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class' : 'input'})
