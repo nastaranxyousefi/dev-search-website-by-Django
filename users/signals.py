b1fafc439c7e6cad5c2e2edffc2f70954f9d3340
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-13 20:22:45 +0430

    add changes to signals.py file in users app

--- users/signals.py
+++ users/signals.py
@@ -7,18 +7,12 @@ from .models import Profile
 def create_profile(sender, instance, created, **kwargs):
     if created:
         user = instance
+        full_name = user.first_name + ' ' + user.last_name
         profile = Profile.objects.create(
             user=user,
             username=user.username,
-        )
-    else:
-        user = instance
-        full_name = user.first_name + ' ' + user.last_name
-        profile = Profile.objects.filter(username__exact=user.username).update(
-            user=user,
-            username=user.username,
-            email=user.email,
             name=full_name,
+            email=user.email,
         )
 
 post_save.connect(create_profile, sender=User)
