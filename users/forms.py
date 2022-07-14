b57c4560b83e40cc102f57820a932c3a90978360
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-14 22:59:13 +0430

    add ProfileForm in forms.py

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+from .models import Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,3 +15,9 @@ class CustomUserCreationForm(UserCreationForm):
         super(CustomUserCreationForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class' : 'input'})
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
--- users/urls.py
+++ users/urls.py
@@ -5,9 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.profiles, name='profiles'),
     path('profile/<str:pk>/', views.user_profile, name='user-profile'),
+
     path('login/', views.login_page, name='login'),
     path('logout/', views.logout_user, name='logout'),
     path('register/', views.register_user, name='register'),
+
     path('my-account/', views.user_account, name='my-account'),
     path('edit-account/', views.edit_account, name='edit-account'),
 
