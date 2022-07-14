6cd0ea3aa977fec74a503b679b7dcf4a45373375
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-15 00:35:38 +0430

    add update_user in signals.py

--- users/signals.py
+++ users/signals.py
@@ -18,6 +18,26 @@ def create_profile(sender, instance, created, **kwargs):
 
 post_save.connect(create_profile, sender=User)
 
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created == False:
+        try:
+            user.first_name = profile.name.split()[0]
+        except:
+            pass
+
+        try:
+            user.last_name = profile.name.split()[1]
+        except:
+            pass
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+post_save.connect(update_user, sender=Profile)
+
 
 @receiver(post_delete, sender=Profile)
 def delete_user(sender, instance, **kwargs):
