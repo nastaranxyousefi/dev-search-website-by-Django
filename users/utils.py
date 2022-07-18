4c9b7a1ccd283721695522d6d6778d2384d849b4
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-18 17:54:18 +0430

    add search_profiles function in utils.py

--- users/utils.py
+++ users/utils.py
@@ -3,3 +3,12 @@ from django.db.models import Q
 from .models import Profile, Skill
 
 
+def search_profiles(request):
+    search_query = request.GET.get('q') or ''
+    skills = Skill.objects.filter(name__icontains=search_query)
+    profiles = Profile.objects.distinct().filter(
+        Q(name__icontains=search_query) |
+        Q(short_intro__icontains=search_query) |
+        Q(skill__in=skills)
+    )
+    return search_query, profiles
--- users/views.py
+++ users/views.py
@@ -73,13 +73,7 @@ def register_user(request):
 
 
 def profiles(request):
-    search_query = request.GET.get('q') or ''
-    skills = Skill.objects.filter(name__icontains=search_query)
-    profiles = Profile.objects.distinct().filter(
-       Q(name__icontains=search_query) |
-       Q(short_intro__icontains= search_query) |
-       Q(skill__in=skills)
-    )
+
     context = {
         'profiles' : profiles,
         'search_query' : search_query,
