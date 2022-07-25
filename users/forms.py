b3e13b2946d4920cd0512d763d1e1538b4777bfd
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-25 23:41:56 +0430

    add MessageForm

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 
-from .models import Profile, Skill
+from .models import Profile, Skill,Message
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -38,5 +38,16 @@ class SkillForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(SkillForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class' : 'input'})
+
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
             field.widget.attrs.update({'class' : 'input'})
