eca58361249cdfb923097c1148dfca1bda39e6c6
nastaranxyousefi <nastaranxyousefi@gmail.com>
2022-07-29 14:03:55 +0430

    complete password reset process

--- users/signals.py
+++ users/signals.py
@@ -1,6 +1,8 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.core.mail import send_mail
+from django.conf import settings
 
 from .models import Profile
 
